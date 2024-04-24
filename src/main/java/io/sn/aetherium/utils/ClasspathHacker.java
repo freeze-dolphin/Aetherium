e33eff9322520368335b588fb371080cfcae2f83
Sapphira Tempest <freeze-dolphin@outlook.com>
2024-04-24 23:32:41 +0800

    refactor: remove debugging println

--- src/main/java/io/sn/aetherium/utils/ClasspathHacker.java
+++ src/main/java/io/sn/aetherium/utils/ClasspathHacker.java
@@ -1,10 +1,8 @@
 package io.sn.aetherium.utils;
 
-import io.ktor.server.application.Application;
 import io.sn.aetherium.objects.AetheriumShardKt;
 import sun.misc.Unsafe;
 
-import javax.security.auth.callback.Callback;
 import java.io.File;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
@@ -12,14 +10,11 @@ import java.lang.invoke.MethodType;
 import java.lang.reflect.Field;
 import java.net.URLClassLoader;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
 
 public class ClasspathHacker {
 
     static MethodHandles.Lookup lookup;
     static Unsafe unsafe;
-    static List<Callback> callbacks = new ArrayList<>();
 
     static {
         try {
@@ -37,7 +32,7 @@ public class ClasspathHacker {
     public static ClassLoader add(Path path) throws Throwable {
         File file = new File(path.toUri().getPath());
         ClassLoader loader = AetheriumShardKt.class.getClassLoader();
-        System.out.println(loader.getClass().getName());
+        // System.out.println(loader.getClass().getName());
         MethodHandle handle = lookup.findVirtual(URLClassLoader.class, "addURL", MethodType.methodType(void.class, java.net.URL.class));
         handle.invoke(loader, file.toURI().toURL());
         return loader;
