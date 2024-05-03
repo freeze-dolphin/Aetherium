45aa9db870ac2a93ecedc87228e24c6c86b3d177
Sapphira Tempest <freeze-dolphin@outlook.com>
2024-05-03 13:43:54 +0800

    refactor: use proper name for AetheriumConnectionException

--- src/main/kotlin/io/sn/aetherium/objects/exceptions/AetheriumConnectionException.kt
+++ src/main/kotlin/io/sn/aetherium/objects/exceptions/AetheriumConnectionException.kt
@@ -1,5 +1,5 @@
 package io.sn.aetherium.objects.exceptions
 
-class AetheriumHaventConnectException(s: String) : Exception(s) {
+class AetheriumConnectionException(s: String) : Exception(s) {
     constructor() : this("")
 }
